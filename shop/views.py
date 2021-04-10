6aa2c9b76ff45bf7fcf472a853b1a87106eb286b
Micah Tobias <mikkywave@gmail.com>
2021-04-10 17:01:47 +0300

    creating project home page

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from .models import Category, Product
+
 # Create your views here.
+def categories(request):
+    return{
+        'categories': Category.objects.all()
+    }
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'shop/home.html', {'products':products})
